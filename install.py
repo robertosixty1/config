cccf259a587e161855632dc3266e59399ec1d2b4
robertosixty1 <roberto011herminegildo@gmail.com>
2022-09-11 18:39:02 -0300

    Remove my tmux configuration since i don't use it anymore

--- install.py
+++ install.py
@@ -11,8 +11,8 @@ if getenv("USER") == "root":
     run(["sudo", "-u", USER, "python", __file__])
     exit(0)
 
-run(["rm", "-f", f"{HOME}/.tmux.conf"])
+run(["rm", "-f", f"{HOME}/.dummy"])
 
-run(["ln", "-s", f"{CWD}/.tmux.conf", f"{HOME}/.tmux.conf"])
+run(["ln", "-s", f"{CWD}/.dummy", f"{HOME}/.dummy"])
 
 print("INTALLED SUCCESSFULLY!")
