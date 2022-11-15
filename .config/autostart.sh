f7a9f7e2e22395391043f0ae13488089724b6764
robertosixty1 <roberto011herminegildo@gmail.com>
2022-11-15 15:04:56 -0300

    Add config files for my window manager

---
+++ .config/autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function run {
+    if ! pgrep $1 ;
+    then
+        $@&
+    fi
+}
+
+run dunst
+run picom --config ~/.config/picom.conf
+run nm-applet
+run volumeicon
--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 from os import getenv, getcwd, listdir
+from os.path import abspath
 from subprocess import run
 
 HOME = getenv("HOME")
@@ -12,7 +13,9 @@ if getenv("USER") == "root":
     exit(0)
 
 run(["rm", "-f", f"{HOME}/.dummy"])
-
 run(["ln", "-s", f"{CWD}/.dummy", f"{HOME}/.dummy"])
+
+for f in listdir("./.config"):
+    run(["ln", "-s", abspath(f"./.config/{f}"), f"{HOME}/.config/{f}"])
 
 print("INTALLED SUCCESSFULLY!")
